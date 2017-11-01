56ee2d0ae8588f90b3a1d023421bbf7ef0d7e5d4
Gustavo Zaik Freitas <zzzaik21@gmail.com>
2017-11-01 10:33:59 -0200

    Core Trabalho LMS

---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+   return render(request, 'index.html')
+
+def lista_curso(request):
+   return render(request, 'lista_curso.html')
+
+def detalhe_curso(request):
+   return render(request, 'detalhe_curso.html')
+
+def disciplina(request):
+   return render(request, 'disciplina.html')
+
+def noticia(request):
+   return render(request, 'noticia.html')
