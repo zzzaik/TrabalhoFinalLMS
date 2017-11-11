3a7e421b93ca81b520e44085df3ef12e6f1230b3
LukaGabriel <30847748+LukaGabriel@users.noreply.github.com>
2017-11-11 18:46:39 -0200

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -9,3 +9,11 @@ class LoginForm(forms.Form):
         retorno = {'ra': self.cleaned_data['ra'], 'senha': self.cleaned_data['senha']}
         return retorno
 
+class MatriculaForm(forms.Form):
+    nomeCompleto = forms.CharField()
+    celular = forms.CharField()
+    email = forms.Charfield()
+
+    def matricular(self):
+        retorno = {'nomeCompleto': self.cleaned_data['nomeCompleto'], 'celular': self.cleaned_data['celular'], 'email':self.cleaned_data['email']}
+        return retorno
