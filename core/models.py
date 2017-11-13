c4de533afe72856977865da132cfd07969bf41a0
zzzaik <zzzaik21@gmail.com>
2017-11-13 18:54:05 -0200

    Banco Local (Azure não funciona na faculdade)

--- core/models.py
+++ core/models.py
@@ -131,7 +131,7 @@ class Aluno(Usuario):
         Usuario, primary_key=True, db_column='user_id', parent_link=True)
     curso = models.ForeignKey(Cursos, blank=True, null=False)
     celular = models.CharField('Celular', max_length=11, null=False)
-    semestre = models.IntegerField('semestre', null=False)
+    semestre = models.IntegerField('Semestre', null=False)
 
     def __str__(self):
         return self.nome
--- site_TFM/settings.py
+++ site_TFM/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'site_TFM.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    #'default': { # Banco no postgreSQL
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #   'NAME': 'DB_LMS',
-    #    'USER': 'postgres',
-    #    'PASSWORD': 'LMS_GP_Draco',
-    #    'HOST': 'localhost',
-    #    'PORT': '8000'
-    #},
-    'default': { # Banco no MSSQL
-        'ENGINE': "sql_server.pyodbc",
-        'NAME': "DB_LMS_postgreSQL",
-        'HOST': "db-lms-draco.database.windows.net",
-        'PORT': '1433',
-        'USER': "LMS_DB_admin@db-lms-draco",
-        'PASSWORD': "P@ssw0rd",
+    'default': { # Banco no postgreSQL
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DB_LMS',
+        'USER': 'postgres',
+        'PASSWORD': 'LMS_GP_Draco',
+        'HOST': 'localhost',
+        'PORT': '8000'
     },
+    #'default': { # Banco no MSSQL
+    #    'ENGINE': "sql_server.pyodbc",
+    #    'NAME': "DB_LMS_postgreSQL",
+    #    'HOST': "db-lms-draco.database.windows.net",
+    #    'PORT': '1433',
+    #    'USER': "LMS_DB_admin@db-lms-draco",
+    #    'PASSWORD': "P@ssw0rd",
+    #},
     #'default': { # Banco de testes sqlite 
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
