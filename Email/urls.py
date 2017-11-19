38944ee82fe84c6c05aa10046a8f3a0b104d9dc0
danielguil <30847758+danielguil@users.noreply.github.com>
2017-11-19 17:27:58 -0200

    urls.py
    impotação do contatoemail e obrigado

---
+++ Email/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import *
+
+from views import contato
+from views import obrigado,contatomail
+
+urlpatterns = patterns('',
+
+   url(url(r'^$,arquivo),
+   
+   url(r'^contato/', 'views.contato', name='contato'),
+   url(r'^obrigado/','views.obrigado', name='obrigado'),
+   
+   
+   
+   )
